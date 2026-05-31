f549ed5f7b4e95448c5fe4748cd60cb999aeb54a
bnshivada42 <erensariot15@gmail.com>
2026-05-31 19:29:44 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,27 +4,24 @@ module.exports = {
         PORT: 25565                       // Sunucu portu
     },
     TEST: {
-        JOIN_DELAY: 5000,     // Botlar arası giriş gecikmesi 5 saniyeye çıkarıldı (Sıkışma engellendi)
+        JOIN_DELAY: 6000,     // Ücretsiz proxyler yavaş olduğu için gecikmeyi 6 saniyeye çıkardık
         CHAT_INTERVAL: 1500000 // Tam 25 dakika (ms)
     },
-    // Tamamen sade, ek kelimesiz özel isim listesi
     BOT_NAMES: ['Raffu', 'Nerfixo', 'rootx', 'aslan522', 'Jeb', 'iamdexter', 'Xaneio'],
     
-    // IP Başına 3 Oyuncu Limitini Aşmak İçin Proxy Havuzu
-    // NULL olanlar Render'ın kendi IP'sini kullanır (İlk 3 bot için)
+    // Görseldeki HTTP proxylerine göre IP dağılımı:
     PROXIES: [
-        null, // Raffu -> Normal IP
-        null, // Nerfixo -> Normal IP
-        null, // rootx -> Normal IP
+        null, // Raffu -> Render IP
+        null, // Nerfixo -> Render IP
+        null, // rootx -> Render IP
         
-        'socks5://Kullanici:Sifre@ProxyIP1:Port', // aslan522 -> Proxy 1
-        'socks5://Kullanici:Sifre@ProxyIP1:Port', // Jeb -> Proxy 1
-        'socks5://Kullanici:Sifre@ProxyIP1:Port', // iamdexter -> Proxy 1
+        'http://78.186.185.152:5253', // aslan522 -> Proxy 1 (Görseldeki 1. sıra)
+        'http://78.186.185.152:5253', // Jeb -> Proxy 1
+        'http://78.186.185.152:5253', // iamdexter -> Proxy 1
         
-        'socks5://Kullanici:Sifre@ProxyIP2:Port'  // Xaneio -> Proxy 2 (Farklı IP)
+        'http://176.88.166.174:8080'  // Xaneio -> Proxy 2 (Görseldeki 3. sıra)
     ],
 
-    // Genişletilmiş ve optimize edilmiş mesaj havuzu
     SPAM_MESSAGES: [
         'lag var mı sizde de?',
         'yetkili alımı var mı sunucuda?',
