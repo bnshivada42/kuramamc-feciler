0b74ad91e68a3490b00452df288a8e2389aa5394
bnshivada42 <erensariot15@gmail.com>
2026-05-31 19:43:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,37 +1,28 @@
 module.exports = {
     SERVER: {
-        IP: 'oyna.kuramamc.com.tr',       // Sunucu IP adresi
-        PORT: 25565                       // Sunucu portu
+        IP: 'oyna.kuramamc.com.tr',
+        PORT: 25565
     },
     TEST: {
-        JOIN_DELAY: 6000,     // Ücretsiz proxyler yavaş olduğu için gecikmeyi 6 saniyeye çıkardık
-        CHAT_INTERVAL: 1500000 // Tam 25 dakika (ms)
+        JOIN_DELAY: 7000, // Proxy gecikmeleri olduğu için süreyi biraz esnettik
+        CHAT_INTERVAL: 1500000
     },
     BOT_NAMES: ['Raffu', 'Nerfixo', 'rootx', 'aslan522', 'Jeb', 'iamdexter', 'Xaneio'],
     
-    // Görseldeki HTTP proxylerine göre IP dağılımı:
+    // IP Dağılımı:
+    // 0,1,2: Render IP
+    // 3,4,5: Proxy 1 (SOCKS4)
+    // 6    : Proxy 2 (HTTP)
     PROXIES: [
-        null, // Raffu -> Render IP
-        null, // Nerfixo -> Render IP
-        null, // rootx -> Render IP
-        
-        'http://78.186.185.152:5253', // aslan522 -> Proxy 1 (Görseldeki 1. sıra)
-        'http://78.186.185.152:5253', // Jeb -> Proxy 1
-        'http://78.186.185.152:5253', // iamdexter -> Proxy 1
-        
-        'http://176.88.166.174:8080'  // Xaneio -> Proxy 2 (Görseldeki 3. sıra)
+        null, null, null, // Render
+        'socks4://176.236.37.132:1080', // Proxy 1
+        'socks4://176.236.37.132:1080', // Proxy 1
+        'socks4://176.236.37.132:1080', // Proxy 1
+        'http://149.86.151.14:8080'     // Proxy 2
     ],
 
     SPAM_MESSAGES: [
-        'lag var mı sizde de?',
-        'yetkili alımı var mı sunucuda?',
-        'spawn çok güzel olmuş elinize sağlık',
-        'sa',
-        'admin varmı?',
-        'market fiyatları güzelmiş',
-        'sunucu çok iyiymiş la valla çok sardı',
-        'etiket atmayın rahatsız oluyorum',
-        'sunucuda hile koruması var mı?',
-        'vip fiyatlarına nereden bakıyoruz?'
+        'lag var mı sizde de?', 'yetkili alımı var mı?', 'spawn çok güzel!', 
+        'sa', 'admin varmı?', 'market fiyatları güzelmiş', 'sunucu çok iyiymiş'
     ]
 };
