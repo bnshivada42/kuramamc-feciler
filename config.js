55c68edc136f74af3caa04349d3d6369fbf1d013
bnshivada42 <erensariot15@gmail.com>
2026-05-31 17:43:07 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    SERVER: {
+        IP: 'oyna.kuramamc.com.tr',       // Sunucu IP adresi
+        PORT: 25565           // Sunucu portu
+    },
+    TEST: {
+        JOIN_DELAY: 1800,     // Botlar arası giriş gecikmesi (ms)
+        CHAT_INTERVAL: 1500000 // Tam 25 dakika (ms)
+    },
+    // Tamamen sade, ek kelimesiz özel isim listesi
+    BOT_NAMES: ['Raffu', 'Nerfix', 'rootx', 'aslan562', 'Jeb'],
+    
+    // Genişletilmiş ve optimize edilmiş mesaj havuzu
+    SPAM_MESSAGES: [
+        'lag var mı sizde de?',
+        'yetkili alımı var mı sunucuda?',
+        'spawn çok güzel olmuş elinize sağlık',
+        'sa',
+        'admin varmı?',
+        'market fiyatları güzelmiş',
+        'sunucu çok iyiymiş la valla çok sardı',
+        'etiket atmayın rahatsız oluyorum',
+        'sunucuda hile koruması var mı?',
+        'vip fiyatlarına nereden bakıyoruz?'
+    ]
+};
